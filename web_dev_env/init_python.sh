e3029febf172a29b95a37693c5e8375e87b4fea4
zs1621 <zs1213yh@gmail.com>
2014-06-18 13:58:56 +0800

    fix python init

--- web_dev_env/init_python.sh
+++ web_dev_env/init_python.sh
@@ -8,3 +8,22 @@ sudo apt-get install python-setuptools
 echo "安装pip"
 
 easy_install pip
+
+echo "change pip source to douban"
+
+if [ -e $HOME/.pip/pip.conf ]; then
+    echo  "$HOME/.pip/pip.conf has exist"
+else
+    echo "
+[global] 
+index-url=http://pypi.douban.com/simple
+    " >> $HOME/.pip/pip.conf
+fi
+
+
+#install virtualenv 
+echo "install virtualenv"
+
+sudo pip install virtualenv
+
+
