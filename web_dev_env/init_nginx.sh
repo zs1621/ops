e1664aba7d1f8868050f29fd43bc00123aef2025
zs1621 <zs1213yh@gmail.com>
2014-08-26 12:37:52 +0800

    fix

---
+++ web_dev_env/init_nginx.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env/bash
+
+
+############################
+# install nginx
+#############################
+
+nginx_version="1.4.3"
+
+
+sudo apt-get -y install libpcre3-dev zlib1g-dev libssl-dev
+
+
+prefix="/srv/nginx"
+
+if  T
