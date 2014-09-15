d757eac5056f19e763c1cdc7c26ef0e128ba8dfe
zs1621 <zs1213yh@gmail.com>
2014-09-15 21:10:09 +0800

    init supervisor

---
+++ web_dev_env/init_supervisor.sh
@@ -0,0 +1,20 @@
+# install supervisor (pip > 1.4, pip --version)
+pip install supervisor --pre
+# default config file 
+echo_supervisord_conf > /etc/supervisord.conf
+
+# download server script  auto init
+curl -O https://gist.githubusercontent.com/zs1621/dc053e0de62f669c1899/raw/7c13a3925ed81abbc81b8fa30c98f9c91f26f2c9/supervisord 
+
+cp supervisord /etc/init.d/
+
+chmod +x /etc/init.d/supervisord
+
+# join system server
+update-rc.d supervisord defaults
+
+# start
+service supervisord start
+
+ps aux | grep supervisor
+
